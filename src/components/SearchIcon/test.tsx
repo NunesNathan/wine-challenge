4ac3959a1990a41ed0511d759c47dd5afc7e2954
NunesNathan <nunesxd7@hotmail.com>
2022-07-02 00:04:30 -0300

    feat: search icon

---
+++ src/components/SearchIcon/test.tsx
@@ -0,0 +1,13 @@
+import { screen } from '@testing-library/react';
+import renderRight from 'utils/test';
+import SearchIcon from '.';
+
+describe('<SearchIcon />', () => {
+  it('should render the SearchIcon', () => {
+    renderRight(<SearchIcon />);
+
+    const searchIcon = screen.getByLabelText('Icone de lupa para pesquisas.');
+
+    expect(searchIcon).toBeInTheDocument();
+  });
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import Filter from 'components/Filter';
 import CartIcon from 'components/CartIcon';
 import Logo from 'components/Logo';
 import ProfileIcon from 'components/ProfileIcon';
+import SearchIcon from 'components/SearchIcon';
 
 export default function Home() {
   return (
     <>
+      <SearchIcon />
       <ProfileIcon />
       <Logo />
       <CartIcon />
